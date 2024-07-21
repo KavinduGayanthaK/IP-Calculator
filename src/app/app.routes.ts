37a889476fb4f87f5d45b98ad1f33aba7ed4eac9
Kavindu <gayanthakavindu3@gmail.com>
2024-07-21 19:05:59 +0530

    created routin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AllocateComponent } from './pages/allocate/allocate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"allocate",
+        component:AllocateComponent
+    }
+];
